30295ed45c4448daa07a8e91bbb588d7d7e3a4dc
fx0883 <jackfeng8123@gmail.com>
2020-08-01 17:19:14 +0800

    123
    1213123

---
+++ KidRecipe/app/src/main/java/com/ChildHealthDiet/app2/constant/Constant.java
@@ -0,0 +1,119 @@
+package com.ChildHealthDiet.app2.constant;
+
+import android.content.Context;
+import android.os.Environment;
+
+import android.util.Log;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+public class Constant {
+//    public static String BASE_IMAGE_URL = "http://192.168.0.100/colorbook/api/";
+//    //    public static String BASE_IMAGE_URL = "http://192.168.0.107/colorbook/api/";
+//    public static String UPLOAD_URL = "http://192.168.0.100/colorbook/uploads/";
+
+//    public static String BASE_IMAGE_URL = "http://templatevilla.net/codecanyon/colorbookadmin/admin/api/";
+//
+//    public static String UPLOAD_URL =  "http://templatevilla.net/codecanyon/colorbookadmin/admin/uploads/";
+
+//    public static String BASE_IMAGE_URL = "http://192.168.31.205:8090/images";
+//    public static String BASE_URL = "http://192.168.31.205:8090";
+//    public static String UPLOAD_URL =  "http://192.168.31.205/uploads/";
+    public static Boolean DEBUG = true;
+    public static String BASE_IMAGE_URL = "http://39.96.168.2:8090/images";
+    public static String BASE_URL = "http://39.96.168.2:8090";
+    public static String UPLOAD_URL =  "http://39.96.168.2/uploads/";
+//    public static String BASE_URL_USER = "http://192.168.31.206:8000";
+////    public static String BASE_URL_USER = "http://localhost.charlesproxy.com:8000";
+//    public static String BASE_URL_USER_AVATAR = "http://192.168.31.206:8000/upload/avatar/";
+//    public static String BASE_URL_UPLOAD_IMAGES = "http://192.168.31.206:8000/upload/color_images/";
+
+    public static String BASE_URL_USER = "http://47.101.48.98/";
+    //    public static String BASE_URL_USER = "http://localhost.charlesproxy.com:8000";
+    public static String BASE_URL_USER_AVATAR = "http://47.101.48.98/upload/avatar/";
+    public static String BASE_URL_UPLOAD_IMAGES = "http://47.101.48.98/upload/color_images/";
+
+
+
+    public static int APP_ID = 1;
+
+
+
+//    public static String BASE_IMAGE_URL = "http://47.101.48.98/api/";
+//
+//    public static String UPLOAD_URL =  "http://47.101.48.98/uploads/";
+    public static String THUMB_URL = UPLOAD_URL + "thumbs/";
+    public static String SAVED_WORK_PATH = Environment.getExternalStorageDirectory() + "/ColorBook/Work/";
+//public static String SAVED_WORK_PATH = Environment.getExternalStorageDirectory() + "/GDTDOWNLOAD/";
+
+    public static String DEFAULT_COLOR = "#F02640";
+    public static int HOME_IMG_SIZE = 6;
+    public static int PAGINATION_SIZE = 10;
+    public static String MY_PREFS = "my_prefs";
+    public static String COLOR = "set_color";
+
+
+
+    public static String SERVICE_AGREEMENT_URL = "http://fx0883.github.io/MySite/cg_fuwuxiyi.html";
+
+    public static String PRIVACY_POLICY_URL = "http://fx0883.github.io/MySite/cg_privacy_policy.html";
+
+    public static String ABOUT_URL = "http://fx0883.github.io/MySite/cg_about.html";
+
+
+    public static String getSavedImgPath(Context context) {
+        return context.getFilesDir().getAbsolutePath() + "/ColorBookMain/";
+    }
+
+    public static String getSavedCatImgPath(Context context) {
+        return context.getFilesDir().getAbsolutePath() + "/ColorBookCat/";
+    }
+
+    public static boolean imgSavedOrNot() {
+        return false;
+    }
+
+
+    public static boolean workImgSavedOrNot(String img_name, String path) {
+        if (img_name != null) {
+            File dir = new File(path);
+            if (dir.exists()) {
+                File[] files = dir.listFiles();
+                for (File file : files) {
+                    Log.e("same==", "" + img_name + "==" + file.getAbsolutePath());
+
+                    if (img_name.equals(file.getAbsolutePath())) {
+                        Log.e("match==", "true");
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+
+
+    public static List<String> getAllCreationList() {
+        List<String> fileList = new ArrayList<>();
+        File dir = new File(SAVED_WORK_PATH);
+        if (dir.exists()) {
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                fileList.add(file.getAbsolutePath());
+            }
+            return fileList;
+        }
+        return fileList;
+    }
+
+
+
+
+}
---
+++ KidRecipe/app/src/main/java/com/ChildHealthDiet/app2/constant/ConstantAd.java
@@ -0,0 +1,19 @@
+package com.ChildHealthDiet.app2.constant;
+
+public class ConstantAd {
+//    public static final String APPID = "2882303761517518052";
+////    public static final String BannerPosID = "802e356f1726f9ff39c69308bfd6f06a";
+
+    public static final String APPID = "2882303761517964032";
+    public static final String BannerPosID = "b3f5d63f5af970f16718c29386fa7a7b";
+    public static final String InterteristalPosID = "ca6de78f3cb76368f4b2a99a773b5129";
+
+    public static final String SplashPosID = "cfe0b8cb48a1706d279831ab3c83dd5e";
+    public static final String NativePosID = "";
+    public static final String NativeVideoPosID = "5090421627704602";
+    public static final String NativeExpressPosID = "7030020348049331"; //如果选择支持视频的模版样式，请使用NativeExpressSupportVideoPosID测试广告位拉取
+    public static final String NativeExpressSupportVideoPosID = "2000629911207832"; //支持视频模版样式的广告位
+    public static final String ContentADPosID = "5060323935699523";
+    public static final String RewardVideoADPosIDSupportH = "2090845242931421";//支持竖版出横版视频
+    public static final String RewardVideoADPosIDUnsupportH = "5040942242835423";//不支持竖版出横版视频
+}
